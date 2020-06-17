a4514b2afa112bad7e9b45bc7ab200cbd3501cd6
Davi <davicpinheiro123@hotmail.com>
2020-06-17 13:25:17 -0300

    Space: Space Parallex criado

---
+++ Assets/Scripts/Space.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Space : MonoBehaviour
+{
+    Material meterialFundo;
+	Vector2 offset;
+	PlayerController player;
+    [SerializeField]
+	float velocity;
+	// Use this for initialization
+	void Start () {
+		meterialFundo = GetComponent<Renderer>().material;
+		player = FindObjectOfType (typeof(PlayerController)) as PlayerController;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, transform.position.z);
+
+		offset += player.GetComponent<Rigidbody2D>().velocity * Time.deltaTime * 0.001f * velocity;
+		meterialFundo.SetTextureOffset ("_MainTex", offset);
+	}
+}
