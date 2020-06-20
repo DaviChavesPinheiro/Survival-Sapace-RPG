ab23db22e3744db6044a2775695dd5bba8b58ce5
Davi <davicpinheiro123@hotmail.com>
2020-06-19 23:56:01 -0300

    Save: Serializable semi automatico usando o BinaryFormatter do c#

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using UnityEngine;
 
@@ -15,9 +16,8 @@ namespace RPG.Saving
             using (FileStream stream = File.Open(path, FileMode.Create))
             {
                 Transform playerTransform = GetPlayerTransform();
-                byte [] buffer = SerializeVector(playerTransform.position);
-
-                stream.Write(buffer, 0, buffer.Length);
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, new SerializableVector3(playerTransform.position));
             }
         }
         public void Load(string saveFile){
@@ -25,12 +25,11 @@ namespace RPG.Saving
             print("Loading to " + path);
             using (FileStream stream = File.Open(path, FileMode.Open))
             {
-                byte[] buffer = new byte[stream.Length];
-                stream.Read(buffer, 0, buffer.Length);
                 Transform playerTransform = GetPlayerTransform();
-                playerTransform.position = DeserializeVector(buffer);
-
-                print(Encoding.UTF8.GetString(buffer));
+                BinaryFormatter formatter = new BinaryFormatter();
+                SerializableVector3 serializableVector3 = (SerializableVector3)formatter.Deserialize(stream);
+                
+                playerTransform.position = serializableVector3.ToVector();
             }
         }
 
@@ -41,23 +40,6 @@ namespace RPG.Saving
 
         private string GetPathFromSaveFile(string saveFile){
             return Path.Combine(Application.persistentDataPath, saveFile + ".sav");
-        }
-        private byte[] SerializeVector(Vector3 vector)
-        {
-            byte [] vectorBytes = new byte[3 * 4];
-            BitConverter.GetBytes(vector.x).CopyTo(vectorBytes, 0);
-            BitConverter.GetBytes(vector.y).CopyTo(vectorBytes, 4);
-            BitConverter.GetBytes(vector.z).CopyTo(vectorBytes, 8);
-            return vectorBytes;
-        }
-
-        private Vector3 DeserializeVector(byte[] buffer)
-        {
-            Vector3 result = new Vector3();
-            result.x = BitConverter.ToSingle(buffer, 0);
-            result.y = BitConverter.ToSingle(buffer, 4);
-            result.z = BitConverter.ToSingle(buffer, 8);
-            return result;
         }
         
     }
