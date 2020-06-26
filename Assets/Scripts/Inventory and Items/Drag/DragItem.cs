7be0dfe0bc2f41bc93552ab31fb0f77f0a788e07
Davi <davicpinheiro123@hotmail.com>
2020-06-26 03:30:58 -0300

    DragItem criado: Agr os items pode se mover (ainda sem o dragAndDrop)

---
+++ Assets/Scripts/Inventory and Items/Drag/DragItem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragItem : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    Canvas canvas;
+    RectTransform rectTransform;
+
+    private void Awake() {
+        canvas = GetComponentInParent<Canvas>();
+        rectTransform = GetComponent<RectTransform>();
+
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        print(eventData.position);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+
+}
