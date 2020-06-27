4a4eb8946ff8ae24dc1060844c846e30788c1317
Davi <davicpinheiro123@hotmail.com>
2020-06-27 18:04:23 -0300

    Criei o PanelControl para o gerenciamento dos subPanels

---
+++ Assets/Scripts/UI/Panel/PanelSlot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelSlot : MonoBehaviour
+{
+    int activeChild = 0;
+
+    public void ActiveUI(int child){
+        if(activeChild == child) return;
+        
+        transform.GetChild(child).gameObject.SetActive(false);
+        activeChild = child;
+        transform.GetChild(child).gameObject.SetActive(true);
+    }
+}
+
+public enum RightSlot{
+    CraftUI,
+    CraftTableUI,
+    EquipUI,
+    ChestUI
+}
+
+public enum LeftSlot{
+    PlayerInventoryUI
+}
---
+++ Assets/Scripts/UI/Panel/PanelUIControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelUIControl : MonoBehaviour
+{
+    [SerializeField] PanelSlot rightSlot;
+    [SerializeField] PanelSlot leftSlot;
+
+    private void OnEnable() {
+        rightSlot.ActiveUI((int) RightSlot.CraftUI);
+        leftSlot.ActiveUI((int) LeftSlot.PlayerInventoryUI);
+    }
+}
