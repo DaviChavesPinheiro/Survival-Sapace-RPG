445b736bcc9a52cc78e205df646f1c83fd84d26c
Davi <davicpinheiro123@hotmail.com>
2020-07-09 21:44:32 -0300

    DropSystem: Agr os objetos do inventory sao dropados

--- Assets/Scripts/Attributes/DropSystem.cs
+++ Assets/Scripts/Attributes/DropSystem.cs
@@ -10,20 +10,30 @@ public class DropSystem : MonoBehaviour
     [SerializeField] List<Item> drops = new List<Item>();
 
     private void OnEnable() {
-        GetComponent<Health>().onDie += OnDie;
+        GetComponent<Health>().onDie += Drop;
     }
 
     private void OnDisable() {
-        GetComponent<Health>().onDie -= OnDie;
+        GetComponent<Health>().onDie -= Drop;
     }
 
-    private void OnDie()
+    private void Drop()
     {
         foreach (Item item in drops)
         {
             GameObject dropObj = Instantiate(dropPrefab, transform.position, transform.rotation);
             dropObj.transform.position = new Vector3(Mathf.FloorToInt(transform.position.x) + UnityEngine.Random.value, Mathf.FloorToInt(transform.position.y) + UnityEngine.Random.value, transform.position.z);
             dropObj.GetComponent<DropController>().SetItem(item, 1);
+            Destroy(dropObj, 60);
+        }
+
+        Slot[] slots = GetComponent<Inventory>()?.GetSlots().ToArray();
+        foreach (Slot slot in slots)
+        {
+            if(slot?.item == null) continue;
+            GameObject dropObj = Instantiate(dropPrefab, transform.position, transform.rotation);
+            dropObj.transform.position = new Vector3(Mathf.FloorToInt(transform.position.x) + UnityEngine.Random.value, Mathf.FloorToInt(transform.position.y) + UnityEngine.Random.value, transform.position.z);
+            dropObj.GetComponent<DropController>().SetItem(slot.item, slot.amount);
             Destroy(dropObj, 60);
         }
     }
