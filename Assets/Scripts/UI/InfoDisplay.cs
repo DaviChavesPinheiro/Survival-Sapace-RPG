dd286c8833946db0a0f690a87edfc9029bb3f134
Davi <davicpinheiro123@hotmail.com>
2020-07-09 21:14:06 -0300

    InfoText

---
+++ Assets/Scripts/UI/InfoDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoDisplay : MonoBehaviour
+{
+    Transform player;
+    Text text;
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        text.text = (new Vector2(Mathf.FloorToInt(player.position.x), Mathf.FloorToInt(player.position.y))).ToString();
+    }
+}
