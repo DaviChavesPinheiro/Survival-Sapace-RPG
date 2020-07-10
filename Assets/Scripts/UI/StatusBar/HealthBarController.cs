edbc74bb723e74cd9e0de0f708c98bbe9376ee45
Davi <davicpinheiro123@hotmail.com>
2020-07-09 21:05:42 -0300

    HealthBar + EnergyBar

---
+++ Assets/Scripts/UI/StatusBar/HealthBarController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarController : StatusBar
+{
+    Health health;
+
+    private void Awake() {
+        health = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+    }
+
+    private void Start() {
+		SetMaxHealth(health.maxHealth);
+		SetHealth(health.health);
+	}
+
+    private void OnEnable() {
+		health.onHealthChange += OnHealthChange;
+	}
+
+	private void OnDisable() {
+		health.onHealthChange -= OnHealthChange;
+	}
+
+    private void OnHealthChange()
+    {
+        SetHealth(health.health);
+    }
+}
---
+++ Assets/Scripts/UI/StatusBar/StatusBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusBar : MonoBehaviour
+{
+    [SerializeField] Slider sliderBar;
+    
+    protected void SetMaxHealth(float value)
+	{
+		sliderBar.maxValue = value;
+	}
+
+    protected void SetHealth(float value)
+	{
+		sliderBar.value = value;
+	}
+}
