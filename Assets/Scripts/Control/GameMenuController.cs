83faa55254f43f93538af06518ee3f49e79c4218
Davi <davicpinheiro123@hotmail.com>
2020-07-12 19:06:42 -0300

    GameMenuController

---
+++ Assets/Scripts/Control/GameMenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using RPG.Saving;
+
+public class GameMenuController : MonoBehaviour
+{
+    SavingWrapper savingWrapper;
+    private void Awake() {
+        savingWrapper = FindObjectOfType(typeof(SavingWrapper)) as SavingWrapper;
+        gameObject.SetActive(false);
+    }
+
+    public void SaveAndQuit(){
+        Save();
+        SceneManager.LoadScene(0);
+    }
+
+    public void Save(){
+        savingWrapper.Save();
+    }
+}
